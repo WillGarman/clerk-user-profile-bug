8e6318e7a5e5f11435f8e9c82039a9d7c2222435
Will Garman <wgarman702@gmail.com>
2025-09-02 14:48:48 -0700

    slight styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,29 +13,47 @@ export default function Home() {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className={`flex flex-col items-center justify-center h-screen gap-4`}>
+    <div
+      className={`flex items-center justify-between max-w-5xl mx-auto h-screen gap-12`}
+    >
       {/* Render Sign In action only when the user is signed out */}
-      <SignedOut>
-        <SignInButton mode="modal" forceRedirectUrl="/">
-          <Button variant="secondary">Sign in to test</Button>
-        </SignInButton>
-      </SignedOut>
+      <div className="flex flex-col gap-2 w-5/6">
+        <SignedOut>
+          <SignInButton mode="modal" forceRedirectUrl="/">
+            <Button variant="secondary" className="w-5/6">
+              Sign in to test
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        {/* Render Sign Out action only when the user is signed in */}
+        <SignedIn>
+          <SignOutButton>
+            <Button variant="secondary" className="w-5/6">
+              Sign Out
+            </Button>
+          </SignOutButton>
+        </SignedIn>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button disabled={!isSignedIn} className="w-5/6">
+              Open user profile dialog
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="min-w-fit p-0">
+            <UserProfile />
+          </DialogContent>
+        </Dialog>
+      </div>
 
-      {/* Render Sign Out action only when the user is signed in */}
-      <SignedIn>
-        <SignOutButton>
-          <Button variant="secondary">Sign Out</Button>
-        </SignOutButton>
-      </SignedIn>
-
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button disabled={!isSignedIn}>Open user profile dialog</Button>
-        </DialogTrigger>
-        <DialogContent className="min-w-fit p-0">
-          <UserProfile />
-        </DialogContent>
-      </Dialog>
+      <div className="flex flex-col gap-2 w-full">
+        Demo of bug:
+        <video
+          src="https://github.com/user-attachments/assets/b7f626ec-aebc-4748-b3fa-227132c30017"
+          autoPlay
+          controls
+          className="rounded-md max-h-[500px]"
+        />
+      </div>
     </div>
   );
 }
