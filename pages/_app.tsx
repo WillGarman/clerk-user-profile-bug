998a6e7ceb35fdf0364ee8af325f4c9704f64bc3
Will Garman <wgarman702@gmail.com>
2025-09-02 14:30:24 -0700

    init clerk and shadcn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
+    throw new Error("Missing CLERK_PUBLISHABLE_KEY");
+  }
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
